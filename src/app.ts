da2be3b6405fc47e7e30c01b6e232518e48d05da
PaulDelamare <paulodela2006@gmail.com>
2026-05-03 15:01:25 +0200

    build: add nodemailer and structure app configs

--- src/app.ts
+++ src/app.ts
@@ -2,21 +2,29 @@
 import express from 'express';
 
 // ! Routes Imports
-import helloRoutes from './Routes/hello.routes';
-import healthRoutes from './Routes/health/health.routes';
-import configureMiddleware from './Configs/apiConfigMiddleware.config';
+import helloRoutes from './modules/core/hello.routes';
+import healthRoutes from './modules/core/health/health.routes';
+import telemetryRoutes from './modules/iot/routes/telemetry.routes';
+import authRoutes from './modules/identity/routes/auth.routes';
+import configureMiddleware from './shared/configs/apiConfigMiddleware.config';  
 
+
+// ! Imports Swagger
+import swaggerUi from 'swagger-ui-express';
+import swaggerSpec from './shared/configs/swagger.config';
 
 // ! Middleware
 const app = express();
 
 configureMiddleware(app);
 
+// ! Swagger Route (Accessible par les front-ends, exp: http://localhost:3000/api-docs)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 
 // ! Routes
+app.use('/api', authRoutes);
 app.use('/api', helloRoutes);
 app.use('/api', healthRoutes);
-
-
-// ! EXPORT
+app.use('/api', telemetryRoutes);
 export { app };
--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,28 @@
 // ! IMPORTS
 import { app } from './app';
-import { bdd } from './Configs/prismaClient.config';
+import { bdd } from './shared/configs/prismaClient.config';
+import { connectMongoDB, disconnectMongoDB } from './shared/configs/mongoClient.config';
 
 // ! CONFIG
 const PORT = process.env.PORT || 3000;
 const API_URL = process.env.API_URL;
 
 // ! INITIALISATION
-const server = app.listen(PORT, () => {
-    console.log(`Server running on ${API_URL}`);
-});
+connectMongoDB()
+    .then(() => {
+        const server = app.listen(PORT, () => {
+            console.log(`Server running on ${API_URL}`);
+        });
 
-// ! FERMETURE
-process.on('SIGINT', async () => {
-    console.log("Server shutdown...");
-    await bdd.$disconnect();
-    server.close(() => {
-        console.log("Server stopped successfully.");
-        process.exit(0);
-    });
-});
+        // ! FERMETURE
+        process.on('SIGINT', async () => {
+            console.log("Server shutdown...");
+            await bdd.$disconnect();
+            await disconnectMongoDB();
+            server.close(() => {
+                console.log("Server stopped successfully.");
+                process.exit(0);
+            });
+        });
+    })
+    .catch(console.error);
