24cd7aa5623ae675182289f668506c07b123fbc6
PaulDelamare <paulodela2006@gmail.com>
2026-05-08 15:32:51 +0200

    clean: suppression de l'ancien dossier monolythique src externe

--- src/Configs/prismaClient.config.ts
+++
@@ -1,6 +0,0 @@
-// ! IMPORTS
-import { PrismaClient } from "@prisma/client";
-
-// ! EXPORT
-// Export du client Prisma
-export const bdd = new PrismaClient();
--- src/Utils/validateData/validateData.model.ts
+++
@@ -1,7 +0,0 @@
-// ! TYPE
-export interface ValidationMessage {
-    field: string;
-    rule: string;
-    message: string;
-    meta?: Record<string, any>;
-}
